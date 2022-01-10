00da477fec545fff89b30e1dd03016e2c1abd299
nicholastonelli <nicholasevantonelli@gmail.com>
2022-01-10 12:14:18 -0500

    added shopfront display

--- controllers/items.js
+++ controllers/items.js
@@ -3,6 +3,22 @@ const express = require("express")
 const router = express.Router()
 const Items = require("../models/items")
 
+const rarityChart = [
+  "common",
+  "uncommon",
+  "rare",
+  "very rare",
+  "legendary",
+  "artifact",
+]
+const priceChart = [
+  [50, 100],
+  [101, 500],
+  [501, 5000],
+  [5001, 50000],
+  [50001, 100000],
+]
+
 router.get("/", (req, res) => {
   Items.find({}, (err, items) => {
     res.render("index", { items })
@@ -15,8 +31,16 @@ router.get("/new", (req, res) => {
 
 // Outfit
 
-router.get('/outfit', (req, res) => {
-  res.render('outfit', { Items: Items})
+router.get("/outfit", (req, res) => {
+  res.render("outfit", { Items: Items })
+})
+
+// Shop
+
+router.get("/shop", (req, res) => {
+  Items.find({}, (err, items) => {
+    res.render("shop", { items, rarityChart, priceChart })
+  })
 })
 
 //New Item
@@ -35,41 +59,37 @@ router.post("/", (req, res) => {
 // Show
 
 router.get("/:id", (req, res) => {
-    Items.findById(req.params.id, (err, item) => {
-      res.render("show", { item })
-    })
+  Items.findById(req.params.id, (err, item) => {
+    res.render("show", { item })
   })
+})
 
 // Edit and Update
 
 router.put("/:id", (req, res) => {
-    if (req.body.attunement === "on") {
-      req.body.attunement = true
-    } else {
-      req.body.attunement = false
+  if (req.body.attunement === "on") {
+    req.body.attunement = true
+  } else {
+    req.body.attunement = false
+  }
+  Items.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedModel) => {
+      res.redirect("/items")
     }
-    Items.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true },
-      (err, updatedModel) => {
-        res.redirect("/items")
-      }
-    )
-  })
+  )
+})
 
 router.get("/:id/edit", (req, res) => {
-    Items.findById(req.params.id, (err, foundItem) => {
-
-      res.render("edit.ejs", {
-        item: foundItem, id: req.params.id
-      })
+  Items.findById(req.params.id, (err, foundItem) => {
+    res.render("edit.ejs", {
+      item: foundItem,
+      id: req.params.id,
     })
   })
-
-
-
-
+})
 
 // Delete Item
 
